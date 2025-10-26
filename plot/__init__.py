bd0885ada1e43caac8b412da6649158e7132d93e
irias <mrdidnew@gmail.com>
2025-10-26 19:36:43 +0300

    for PETIA

--- plot/__init__.py
+++ plot/__init__.py
@@ -7,22 +7,24 @@ def differential_equation(x, y):
     """
     y[0] = y(x)
     y[1] = y'(x)
-    y'' - 6 y' + 8 y = 2 x^2 + x
+    y'' + 6 y' + 8 y = 0
     """
     dydx = np.zeros(2)
     dydx[0] = y[1]
-    dydx[1] = 6*y[1] - 8*y[0] + 2*x**2 + x
+    dydx[1] = 6*y[1] + 8*y[0]
     return dydx
 
 # === Аналитическое решение ===
 def analytical_solution(x):
-    return (-35/8 * np.exp(2*x) + 33/16 * np.exp(4*x) +
-            0.25*x**2 + 0.5*x + 5/16)
+    return () #впиши свое аналитическое решение
 
 # === Параметры ===
-x0 = 0.0
+
+x0 = 0.0 #Выбирай началные точки от куда до куда
 x_end = 2.0
-y0 = np.array([-2.0, 0.0])  # y(0) = -2, y'(0) = 0
+
+
+y0 = np.array([-1.0, 0.0])  # y(0) = -1, y'(0) = 0
 
 # === Численное решение через scipy.solve_ivp ===
 x_eval = np.linspace(x0, x_end, 1000)
